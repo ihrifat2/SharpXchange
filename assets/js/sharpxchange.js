aa04e828344bdbb6c3e706250422170d9c410e0c
Imran Hadid <imranhadid03@gmail.com>
2019-02-10 00:47:08 +0600

    10-2-19

--- assets/js/sharpxchange.js
+++ assets/js/sharpxchange.js
@@ -1,3 +1,24 @@
-$('.carousel').carousel({
-    interval: 1000
-});
+function checkSendUsInput() {
+    jQuery.ajax({
+        url: "check_ajax.php",
+        data:'sxcSendUs='+$("#sxcSendUs").val(),
+        type: "POST",
+        success:function(data){
+            $("#sxc_imageSendUs").attr("src","assets/img/"+data+".png");
+        },
+        error:function (){}
+    });
+}
+
+function checkRecieveInput() {
+    jQuery.ajax({
+        url: "check_ajax.php",
+        data:'sxcRecieve='+$("#sxcRecieve").val(),
+        type: "POST",
+        success:function(data){
+            // $("#sxc_imageRecieveStatus").html(data);
+            $("#sxc_imageRecieve").attr("src","assets/img/"+data+".png");
+        },
+        error:function (){}
+    });
+}
