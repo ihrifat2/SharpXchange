6f4a9d9fbacb59e330ce2804e66955aaf9f0c98b
Imran Hadid <imranhadid03@gmail.com>
2019-04-01 23:45:39 +0600

    add live Chat service

--- testimonial.php
+++ testimonial.php
@@ -237,6 +237,15 @@ $username = $_SESSION['user_login_session'];
         function testimonialid($data) {
             document.cookie = "tstmonID="+$data;
         }
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/5ca249e21de11b6e3b064991/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
     </script>
 </body>
 </html>
