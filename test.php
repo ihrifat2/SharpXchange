77d165267427528a3664d457a52f9935a9d7d515
Imran Hadid <imranhadid03@gmail.com>
2019-04-14 00:06:14 +0600

    Update test.php

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-$baseurl = "http://".$_SERVER['SERVER_NAME'] . "dirname($_SERVER['PHP_SELF'])";
-echo $baseurl;
-
-$baseurl2 = "http://".$_SERVER['SERVER_NAME'] . "/sxc/";
-echo $baseurl2;
-?>
