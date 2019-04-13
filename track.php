cc850301bc6025e389af4c6af155c4bd36201a94
Imran Hadid <imranhadid03@gmail.com>
2019-04-14 00:05:32 +0600

    Update track.php

--- track.php
+++ track.php
@@ -11,8 +11,8 @@ function validate_input($data) {
     return $data;
 }
 
-if (isset($_POST['sxcTrackBtn'])) {
-    $exId = $_POST['sxcTrackExID'];
+if (isset($_GET['sxcTrackExID'])) {
+    $exId = $_GET['sxcTrackExID'];
     $exId = validate_input($exId);
     $exId = mysqli_real_escape_string($dbconnect, $exId);
     $sqlQuery   = "SELECT `gateway_sell`, `gateway_recieve`, `amount_sell`, `amount_recieve`, `status`, `date` FROM `tbl_exchange_info` WHERE `transaction_id` = '$exId'";
@@ -31,17 +31,16 @@ if (isset($_POST['sxcTrackBtn'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="Money Exchanger, Dollar Buy and Sell, Trusted Source">
-    <meta name="keywords" content="Money Exchanger, Dollar Buy and Sell, Trusted Source">
-    <meta property="og:url" content="https://www.sharpxchange.com">
-    <meta property="og:title" content="Money Exchanger, Dollar Buy and Sell">
-    <meta property="og:description" content="Money Exchanger, Dollar Buy and Sell, Trusted Source">
-    <meta property="og:image" content="https://www.sharpxchange.com/assets/img/logo.png">
-    <meta property="og:type" content="Website">
+    <meta name="description" content="SharpXchange">
+    <meta name="author" content="Imran Hadid">
+    <meta name="generator" content="Imran">
     <title>SharpXchange</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="http://asset.sharpxchange.com/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://asset.sharpxchange.com/assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="http://asset.sharpxchange.com/assets/css/accountupdate.css">
 
     <style>
         .bd-placeholder-img {
@@ -56,12 +55,12 @@ if (isset($_POST['sxcTrackBtn'])) {
         }
     </style>
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    <script src="assets/js/jquery-3.3.1.min.js"></script>
-    <script src="assets/js/popper.min.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script src="assets/js/sharpxchange.js"></script>
+    <script src="http://asset.sharpxchange.com/assets/js/jquery-3.3.1.min.js"></script>
+    <script src="http://asset.sharpxchange.com/assets/js/popper.min.js"></script>
+    <script src="http://asset.sharpxchange.com/assets/js/bootstrap.min.js"></script>
+    <script src="http://asset.sharpxchange.com/assets/js/sharpxchange.js"></script>
 </head>
-<body class="pagecontainer">
+<body>
     <div class="container">
         <header class="sharpxchange-header py-3">
             <div class="row flex-nowrap justify-content-between align-items-center">
@@ -92,7 +91,7 @@ if (isset($_POST['sxcTrackBtn'])) {
                     }
                 ?>
                 <div class="col-sm-4 col-md-4 text-center">
-                    <a class="sharpxchange-header-logo text-dark" href="index.php">SharpXchange</a>
+                    <a class="sharpxchange-header-logo text-dark" href="/">SharpXchange</a>
                 </div>
                 <div class="col-sm-4 col-md-4 d-flex justify-content-end align-items-center sharpxchange-header-nav-right">
                     <a class="text-muted">
@@ -106,10 +105,10 @@ if (isset($_POST['sxcTrackBtn'])) {
             <header class="masthead mb-auto">
                 <div class="inner">
                     <nav class="nav nav-masthead justify-content-end">
-                        <a class="nav-link" href="index.php">EXCHANGE</a>
-                        <a class="nav-link" href="testimonials.html">TESTIMONIALS</a>
-                        <a class="nav-link active" href="contact.html">CONTACT</a>
-                        <a class="nav-link" href="aboutUs.php">ABOUT US</a>
+                        <a class="nav-link" href="/">EXCHANGE</a>
+                        <a class="nav-link" href="testimonials">TESTIMONIALS</a>
+                        <a class="nav-link active" href="contact">CONTACT</a>
+                        <a class="nav-link" href="about">ABOUT US</a>
                     </nav>
                 </div>
             </header>
@@ -122,7 +121,7 @@ if (isset($_POST['sxcTrackBtn'])) {
         <div class="row justify-content-md-center">
             <main role="main" class="col-md-9 col-lg-9  mt-4">
                 <div class="exchageTrack section4 mb-2">
-                    <form class="exchageTrackSection" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <form class="exchageTrackSection" method="get" action="<?php echo htmlspecialchars("/track");?>">
                         <h5 class="title">Track Exchange</h5>
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Exchange ID" name="sxcTrackExID">
@@ -146,9 +145,9 @@ if (isset($_POST['sxcTrackBtn'])) {
                         <tr>
                             <td colspan="2">
                                 <h2 class="text-center">
-                                    <img src="assets/img/<?php echo datanameToPic($gt_sell); ?>" width="50px" height="50px" class="img-circle">
+                                    <img src="http://asset.sharpxchange.com/assets/img/<?php echo datanameToPic($gt_sell); ?>" width="50px" height="50px" class="img-circle">
                                     <b><?php echo $gt_sell; ?></b><i class="fa fa-refresh ml-4 mr-4"></i>
-                                    <img src="assets/img/<?php echo datanameToPic($gt_rcev); ?>" width="50px" height="50px" class="img-circle">
+                                    <img src="http://asset.sharpxchange.com/assets/img/<?php echo datanameToPic($gt_rcev); ?>" width="50px" height="50px" class="img-circle">
                                     <b><?php echo $gt_rcev; ?></b>
                                 </h2>
                             </td>
@@ -188,16 +187,16 @@ if (isset($_POST['sxcTrackBtn'])) {
                 <div class="col-md-4">
                     <h3>Quick access</h3>
                     <ul>
-                        <li><a href="index.php">Exchanger</a></li>
-                        <li><a href="testimonials.html">Testimonials</a></li>
+                        <li><a href="/">Exchanger</a></li>
+                        <li><a href="testimonials">Testimonials</a></li>
                     </ul>
                 </div>
                 <div class="col-md-4">
                     <h3>Terms & Support</h3>
                     <ul>
-                        <li><a href="policy.php">Privacy Policy</a></li>
-                        <li><a href="aboutUs.php">About Us</a></li>
-                        <li><a href="contact.html">Contact</a></li>
+                        <li><a href="policy">Privacy Policy</a></li>
+                        <li><a href="about">About Us</a></li>
+                        <li><a href="contact">Contact</a></li>
                     </ul>
                 </div>
                 <div class="col-md-4">
@@ -212,19 +211,5 @@ if (isset($_POST['sxcTrackBtn'])) {
         <p class="mt-3">Copyright © 2019. SharpXchange</p>
         <p><a href="#">Back to top</a></p>
     </footer>
-
-    <noscript>
-        <style type="text/css">
-            .pagecontainer {display:none;}
-            html {
-                background-image: url("assets/img/JS.jpg");
-                background-color: #eedb50;
-                background-repeat: no-repeat;
-                background-attachment: fixed;
-                background-position: center;
-            }
-        </style>
-        Your browser does not support JavaScript!
-    </noscript>
 </body>
 </html>
