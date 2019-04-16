a565c0f4bd657ac28be89e5cb8dbc8616e0555dc
Imran Hadid <imranhadid03@gmail.com>
2019-04-16 21:50:16 +0600

    delete css

--- signup.php
+++ signup.php
@@ -7,6 +7,7 @@ require "helper.php";
 require "helpertwo.php";
 require "hash.php";
 require "mail.php";
+require "header.php";
 
 if (isset($_SESSION['user_login_session'])) {
     header('Location: index.php');
@@ -70,7 +71,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['sxc_signup_btn'])) {
                             if ($sxc_signup_paswd != $sxc_signup_conpaswd) {
                                 $error = "Password not matched";
                             } else {
-
 
                                 $sqlQuery   = "SELECT `token_id` FROM `tbl_token` WHERE `email` = '$sxc_signup_email'";
                                 $result     = mysqli_query($dbconnect, $sqlQuery);
@@ -136,18 +136,6 @@ generateSessionToken();
     <link rel="stylesheet" href="https://asset.sharpxchange.com/assets/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
     <script src="https://asset.sharpxchange.com/assets/js/jquery-3.3.1.min.js"></script>
     <script src="https://asset.sharpxchange.com/assets/js/sharpxchange.js"></script>
