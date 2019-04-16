ec903787d4cad748e06200d48084039cc9a4cede
Imran Hadid <imranhadid03@gmail.com>
2019-04-16 21:52:01 +0600

    css delete

--- testimonial.php
+++ testimonial.php
@@ -3,6 +3,7 @@ session_start();
 require 'xsrf.php';
 require "dbconnect.php";
 require "auth.php";
+require "header.php";
 $username = $_SESSION['user_login_session'];
 
 function validate_input($data) {
@@ -83,18 +84,6 @@ generateSessionToken();
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://asset.sharpxchange.com/assets/css/accountupdate.css">
 
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
     <script src="https://asset.sharpxchange.com/assets/js/jquery-3.3.1.min.js"></script>
     <script src="https://asset.sharpxchange.com/assets/js/popper.min.js"></script>
