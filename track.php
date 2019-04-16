a4af014f0a802a8179349dca434b7ea1d415de0e
Imran Hadid <imranhadid03@gmail.com>
2019-04-16 21:56:13 +0600

    Update track.php

--- track.php
+++ track.php
@@ -42,18 +42,6 @@ if (isset($_GET['sxcTrackExID'])) {
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://asset.sharpxchange.com/assets/css/accountupdate.css">
 
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
     <script src="https://asset.sharpxchange.com/assets/js/jquery-3.3.1.min.js"></script>
     <script src="https://asset.sharpxchange.com/assets/js/popper.min.js"></script>
