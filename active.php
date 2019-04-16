5a31c8d88d7b8e6bb4645cbe88532c1f4bbd8e59
Imran Hadid <imranhadid03@gmail.com>
2019-04-16 21:49:56 +0600

    delete css

--- active.php
+++ active.php
@@ -13,18 +13,6 @@
     <link rel="stylesheet" href="https://asset.sharpxchange.com/assets/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-    </style>
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
     <script src="https://asset.sharpxchange.com/assets/js/jquery-3.3.1.min.js"></script>
     <script src="https://asset.sharpxchange.com/assets/js/bootstrap.min.js"></script>
@@ -34,6 +22,7 @@
 session_start();
 require "dbconnect.php";
 require "hash.php";
+require "header.php";
 
 if (!isset($_GET['token']) || !isset($_GET['code'])) {
     header('Location: /error');
