3924a4498c49ef824921c776f3e52db5bbaa0778
Imran Hadid <imranhadid03@gmail.com>
2019-04-20 12:29:10 +0600

    fix issue 2

--- logout.php
+++ logout.php
@@ -2,6 +2,8 @@
 session_start();
 if (isset($_SESSION['user_login_session'] )) {
 	session_destroy();
+	setcookie('SharpXchange_Cookie', '', time() - 3600);
+    echo '<script>document.cookie = "SharpXchange_Cookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";</script>';
 	header('Location: /');
 }else{
 	header('Location: /');
