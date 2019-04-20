dabd986fdd397290c956ef0b65d7e8a371115e0c
Imran Hadid <imranhadid03@gmail.com>
2019-04-20 12:28:04 +0600

    fix issue 2

--- auth.php
+++ auth.php
@@ -1,7 +1,29 @@
 <?php
 
 if(!isset($_SESSION["user_login_session"])){
-	header("Location: signin.php");
+	header("Location: signin");
+}
+
+if ($_SESSION["PHPSESSID"] != $_COOKIE['SharpXchange_Cookie']) {
+	header("Location: logout");
+}
+
+if (isset($_SESSION['PHPSESSID'])) {
+	$phpsessionid = $_SESSION['PHPSESSID'];
+	$countSessionId = getSessionStatus($phpsessionid);
+
+	if($countSessionId == 0) {
+		header("Location: logout");
+	}
+}
+
+function getSessionStatus($data) {
+	require "dbconnect.php";
+	$sqlQuery       = "SELECT COUNT(`session_id`) FROM `tbl_sessiontoken` WHERE `session_token` = '$data'";
+    $result         = mysqli_query($dbconnect, $sqlQuery);
+    $rows           = mysqli_fetch_array($result);
+    $session_id 	= $rows['COUNT(`session_id`)'];
+    return $session_id;
 }
 
 ?>
