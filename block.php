df0317ce3bf3d9a9a7a4c3b4d67c7e89232e5589
Imran Hadid <imranhadid03@gmail.com>
2019-04-26 23:41:15 +0600

    Update block.php

--- block.php
+++ block.php
@@ -41,21 +41,13 @@ require "header.php";
     </div>
 
     <script type="text/javascript">
-        // Set the time 5 minutes in countDownDate variable while blocking ip address
         var countDownDate = new Date().getTime()+301000;
-        //var countDownDate = new Date().getTime()+10000;
-        // Update the count down every 1 second
         var x = setInterval(function() {
-            // Get todays time
             var now = new Date().getTime();
-            // Find the distance between now an the count down date
             var distance = countDownDate - now;
-            // Time calculations for minutes and seconds
             var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            // Output the result in an element with id="demo"
             document.getElementById("demo").innerHTML =  minutes + "m " + seconds + "s ";
-            // If the count down is over, write some text 
             if (distance < 0) {
                 clearInterval(x);
                 document.getElementById("demo").innerHTML = "<font color='red'>EXPIRED</font>";
